91549777abed95e543d3488ab33463913544da2e
YeeunHeo <gjdpdms2005@gmail.com>
2020-07-21 15:58:37 +0900

    tutorials

---
+++ tutorials/nodemon-basic/README.md
@@ -0,0 +1,12 @@
+# Nodemon
+
+- 내용이 업데이트 될 때마다 서버를 재시동하지 않아도 업데이트를 할 수 있게 만들어주는 모듈
+
+```js
+// nodemon 설치
+sudo npm install -g nodemon
+
+// nodemon이 설치가 되지 않았더라도 스스로 설치되면서 실행
+npx nodemon index.js 
+```
+
---
+++ tutorials/nodemon-basic/index.js
@@ -0,0 +1,8 @@
+const http = require('http'); // http (내장 모듈) 불러오기
+
+//서버 생성
+http.createServer((request, response) => {
+    response.writeHead(200, {'Content-Type': 'text/plain'}); // http 번호: 200번, text/plain: 문서의 타입을 지정
+    response.write('Hello Node js'); // 출력 텍스트
+    response.end();
+}).listen(3000); //port번호: 3000번
