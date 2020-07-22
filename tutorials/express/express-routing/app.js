bcb79de9ae367c277bbd22dc5a024dc6a0197cb3
YeeunHeo <gjdpdms2005@gmail.com>
2020-07-22 18:04:49 +0900

    template

--- tutorials/express/express-routing/app.js
+++ tutorials/express/express-routing/app.js
@@ -1,9 +1,16 @@
 const express = require('express');
+const nunjucks  = require('nunjucks');
+
 const admin = require('./routes/admin');
 const contact = require('./routes/contact');
 
 const app = express();
 const port = 3000; // port 번호
+
+nunjucks.configure('template',{
+    autoescape: true, // html 태그 작동여부
+    express: app // express 객체 설정
+});
 
 // url 추가
 app.get('/', (req, res) =>{
--- tutorials/express/express-routing/routes/admin.js
+++ tutorials/express/express-routing/routes/admin.js
@@ -6,7 +6,11 @@ router.get('/', (req, res) => {
 });
 
 router.get('/products', (req, res) => {
-    res.send('admin/products url');
+    // res.send('admin/products url');
+    res.render('admin/products.html', {
+        message: ` <h1>태그가 출력됩니다.</h1>`,
+        online: 'express'
+    }) // 템플릿 전송
 });
 
 module.exports = router;
